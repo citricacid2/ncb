9cbb6725904251b7e91b5091b582217f625632c2
Ayush <paulayush@gmail.com>
2021-09-20 17:59:27 +0000

    chart

--- pages/vote.js
+++ pages/vote.js
@@ -1,30 +1,47 @@
-import Chart from "chart.js/auto"
+import { Doughnut } from 'react-chartjs-2'
+
 import { useEffect, useState } from "react"
 
 export default function Vote() {
-  const [data, setData] = useState({
-    votes: [
-      {name: 'Ayush', votes: 14},
-      {name: 'Paul', votes: 9},
-    ]
-  })
+  const [data, setData] = useState([
+    {name: 'Ayush', votes: 14},
+    {name: 'Paul', votes: 9},
+  ])
+
   return (
     <div className='m-6'>
-      {data.votes.map((item) => 
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter is-full-mobile">
+          <Doughnut data={
+            {
+              labels: data.map(item => item.name),
+              datasets: [{
+                data: data.map(item => item.votes),
+              }]
+            }
+          }/>
+        </div>
+      </div>
+      {data.map((item) => 
         (
-          <div className="columns mt-3" key={item.name}>
-            <div className="column is-one-fifth is-half-mobile">
+          <div className="columns mt-3 is-mobile is-multiline is-vcentered" key={item.name}>
+            <div className="column is-full-mobile is-2">
               <strong>Name: </strong>
               {item.name}
             </div>
-            <div className="column is-one-fifth is-half-mobile">
+            <div className="column is-2 is-half-mobile">
               <strong>Votes: </strong>
               {item.votes}
               {data.total}
             </div>
-            <div className="column is-full-mobile">
-              <progress className="progress is-danger my-auto" value={item.votes} max={data.votes.map(vote => vote.votes).reduce((p, n) => p + n)}>
+            <div className="column">
+              <progress className="progress is-danger my-auto" value={item.votes} max={data.map(vote => vote.votes).reduce((p, n) => p + n)}>
               </progress>
+            </div>
+            <div className="column is-narrow">
+              <button className="button is-dark">
+                Vote
+              </button>
             </div>
           </div>
         )
